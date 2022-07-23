6fdce6f4bf85ed86ca9377823b4365421e757e2a
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-07-23 19:36:49 +0600

    fetch data using axios

---
+++ fetch-data/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todos from "./component/Todos";
+
+
+export default function App() {
+  return (
+    <div>
+        <h1>redux-app</h1>
+        <Todos/>
+    </div>
+  );
+}
